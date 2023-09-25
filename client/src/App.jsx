79303eda7a70c680c7c4ce01fe231d0817190e15
Lerato Codes <111055500+Rato24@users.noreply.github.com>
2023-09-25 19:52:51 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import CanvasModel from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <main className="transition-all ease-in app">
+      <Home />
+      <CanvasModel />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
